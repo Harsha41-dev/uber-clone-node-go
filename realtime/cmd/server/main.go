8ae64d6104ec93277f59ab420eff7781487e326c
SAIHARSHA P <174473155+Harsha41-dev@users.noreply.github.com>
2026-03-20 10:33:43 +0530

    feat: add basic api and realtime starter

---
+++ realtime/cmd/server/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/health", healthHandler)
+
+	log.Println("Realtime service running on port 8080")
+	err := http.ListenAndServe(":8080", withCORS(mux))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func withCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_ = json.NewEncoder(w).Encode(map[string]any{
+		"service": "uber-realtime",
+		"ok":      true,
+	})
+}
